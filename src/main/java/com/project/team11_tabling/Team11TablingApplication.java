e2ba79fffdae57773eb14d369facb0e9c3c55778
mine <mineki94@naver.com>
2024-03-26 20:55:21 +0900

    add : 프로젝트 기본 파일 생성

---
+++ src/main/java/com/project/team11_tabling/Team11TablingApplication.java
@@ -0,0 +1,13 @@
+package com.project.team11_tabling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team11TablingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team11TablingApplication.class, args);
+	}
+
+}
