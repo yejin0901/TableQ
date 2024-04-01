4667711515743d0c38710c45922684d6555b9b6d
yejin0901 <61917664+yejin0901@users.noreply.github.com>
2024-04-01 20:55:45 +0900

    api 요청 security 접근 허가 처리 (#24)
    * feat: shop
    
    * add: 카카오 검색 API
    
    * add: ehcache in open api
    
    * Remove application.properties from Git tracking
    
    * Add application.properties to .gitignore
    
    * add: register shop method
    
    * fix: register shop() method
    
    * merge dev
    
    * fix: open api security 접근 허용
    
    * delete RedisConfig
    
    * chore: gradle 수정

--- src/main/java/com/project/team11_tabling/global/config/WebSecurityConfig.java
+++ src/main/java/com/project/team11_tabling/global/config/WebSecurityConfig.java
@@ -38,6 +38,7 @@ public class WebSecurityConfig {
             authorizeHttpRequests
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
                 .permitAll() // resources 접근 허용 설정
+                .requestMatchers("/api/shops").permitAll()
             .requestMatchers("/api/users/signup").permitAll()
             .requestMatchers("/api/users/login").permitAll()// '/api/user/'로 시작하는 요청 모두 접근 허가
             .anyRequest().authenticated() // 그 외 모든 요청 인증처리
