0137c37efb23e815e73bb3b67df467ebc73db93b
Hako99 <154326391+Hako99@users.noreply.github.com>
2024-04-22 16:55:53 +0900

    Feat/cicd (#56)
    * feat/cicd (add) : CICD 설정 파일 추가
    
    * feat/cicd (fix) : build.gradle 생성 경로 수정
    
    * feat/cicd (fix) : build.gradle 생성 권한 부여
    
    * feat/cicd (fix) : build.gradle 생성 권한 삭제
    
    * feat/cicd (fix) : build.gradle 읽기 쓰기 권한 부여
    
    * feat/cicd (fix) : build.gradle 생성방식 변경
    
    * feat/cicd (add) : cicd 설정 파일들 추가
    
    * feat/cicd (fix) : 오타 수정
    
    * feat/cicd (fix) : 버킷 이름 수정
    
    * feat/cicd (fix) : 코드 디플로이 재설정
    
    * feat/cicd (fix) : 중복코드 주석
    
    * feat/cicd (fix) : 우분투로 수정
    
    * feat/cicd (fix) : 다시 리눅스로
    
    * feat/cicd (fix) : 배포 테스트
    
    * feat/cicd (fix) : 배포 테스트
    
    * feat/cicd (fix) : 배포 테스트
    
    * feat/cicd (fix) : 배포 테스트
    
    * feat/cicd (fix) : 배포 테스트
    
    * feat/cicd (fix) : 배포 테스트
    
    * feat/cicd (fix) : 배포 테스트
    
    * feat/cicd (fix) : cicd 코드 발동 조건 수정
    
    * feat/cicd (fix) : cicd 코드 발동 조건 수정
    
    * feat/cicd (fix) : 필요없는 테스트 코드 삭제
    
    * feat/cicd (fix) : 로드밸런스 테스트
    
    * feat/cicd (fix) : 로드밸런스 테스트
    
    * feat/cicd (fix) : 로드밸런스 테스트 확인 후 코드 복구
    
    * feat/cicd (fix) : 프론트와 연결되는 링크 수정
    
    * feat/cicd (fix) : ec2 인증 요청 코드
    
    * feat/cicd (fix) : CORS 설정 수정

--- src/main/java/com/project/team11_tabling/global/config/WebMvcConfig.java
+++ src/main/java/com/project/team11_tabling/global/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("https://tabling.p-hako.com:3000")
+        .allowedOrigins("https://tabling.p-hako.com")
         .allowedMethods("POST", "PUT", "GET", "DELETE")
     .allowCredentials(true);
   }
