8698413cf623ff1e5a65d7e17d16a38189063b92
Hako99 <154326391+Hako99@users.noreply.github.com>
2024-04-23 20:12:40 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,5 +18,16 @@ else
   sleep 5
 fi
 
-echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > /home/ec2-user/tabling/logs/server.log 2>&1 &
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR NAME: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
