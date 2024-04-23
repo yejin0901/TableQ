5ecc48402b72139af8ea169d27de8ccf278c9659
Hako99 <154326391+Hako99@users.noreply.github.com>
2024-04-23 20:31:00 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,16 +18,5 @@ else
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+nohup java -jar /home/ec2-user/tabling/build/libs/team11_tabling-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev 
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
-echo "> JAR NAME: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
