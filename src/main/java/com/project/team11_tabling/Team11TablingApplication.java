df771efb15c8c93bbd268cc4ea78f31cefcfec21
beunchoi <154890316+beunchoi@users.noreply.github.com>
2024-04-25 20:09:08 +0900

    스프링 배치 분리 (#66)
    * feat : 로그인, 회원가입 프론트 구현
    
    * feat : 유저 회원 등급 자동 업데이트 기능 구현
    
    * Refactor : 예약내역 배치 기능 수정
    
    * feat : 인기매장 자동 업데이트 배치 기능 구현
    
    * Rafactor : 회원 등급 자동 업데이트 기능 수정
    
    * Rafactor : 스케줄러 수정
    
    * Fix : 프론트 삭제
    
    * Refactor : 유저 테스트용 스케줄러 시간 변경
    
    * Refactor : 스프링 배치 분리

--- src/main/java/com/project/team11_tabling/Team11TablingApplication.java
+++ src/main/java/com/project/team11_tabling/Team11TablingApplication.java
@@ -1,13 +1,11 @@
 package com.project.team11_tabling;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableBatchProcessing
 public class Team11TablingApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/project/team11_tabling/global/scheduler/Scheduler.java
+++ src/main/java/com/project/team11_tabling/global/scheduler/Scheduler.java
@@ -2,28 +2,13 @@ package com.project.team11_tabling.global.scheduler;
 
 import com.project.team11_tabling.domain.shop.entity.ShopSeats;
 import com.project.team11_tabling.domain.shop.repository.ShopSeatsRepository;
-import com.project.team11_tabling.global.batch.BookingBatch;
-import com.project.team11_tabling.global.batch.PopularShopBatch;
-import com.project.team11_tabling.global.batch.UserGradeBatch;
 import com.project.team11_tabling.global.event.CallingEvent;
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.JobParameter;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Component
 @Slf4j
@@ -32,14 +17,6 @@ public class Scheduler {
 
   private final ShopSeatsRepository shopSeatsRepository;
   private final ApplicationEventPublisher eventPublisher;
-  private final JobLauncher jobLauncher;
-  private final BookingBatch bookingBatch;
-  private final UserGradeBatch userGradeBatch;
-  private final JobRepository jobRepository;
-  private final PlatformTransactionManager transactionManager;
-  private final DataSource dataSource;
-  private final PopularShopBatch popularShopBatch;
-
 
   @Scheduled(fixedDelay = 30000)
   public void addAvailableSeat() {
@@ -55,62 +32,5 @@ public class Scheduler {
     }
 
     eventPublisher.publishEvent(new CallingEvent());
-  }
-
-
-  @Scheduled(cron = "0 0 1 * * *", zone = "Asia/Seoul")
-  public void weeklyBooking() {
-    Map<String, JobParameter<?>> confMap = new HashMap<>();
-
-    LocalDateTime now = LocalDateTime.now();
-
-    confMap.put("지난주 예약 내역", new JobParameter<>(now, LocalDateTime.class));
-    JobParameters jobParameters = new JobParameters(confMap);
-
-    try {
-      jobLauncher.run(bookingBatch.bookingJob(jobRepository,transactionManager), jobParameters);
-    } catch (JobExecutionAlreadyRunningException | JobInstanceAlreadyCompleteException
-             | JobParametersInvalidException | org.springframework.batch.core.repository.JobRestartException e) {
-
-      log.error(e.getMessage());
-    }
-  }
-
-
-  @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
-  public void weeklyUpdateUser() {
-    Map<String, JobParameter<?>> confMap = new HashMap<>();
-
-    LocalDateTime now = LocalDateTime.now();
-
-    confMap.put("주간 회원 등급 업데이트", new JobParameter<>(now, LocalDateTime.class));
-    JobParameters jobParameters = new JobParameters(confMap);
-
-    try {
-      jobLauncher.run(userGradeBatch.userGradeJob(jobRepository,transactionManager, dataSource), jobParameters);
-    } catch (JobExecutionAlreadyRunningException | JobInstanceAlreadyCompleteException
-             | JobParametersInvalidException | org.springframework.batch.core.repository.JobRestartException e) {
-
-      log.error(e.getMessage());
-    }
-  }
-
-
-  @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
-  public void weeklyPopularShop() {
-    Map<String, JobParameter<?>> confMap = new HashMap<>();
-
-    LocalDateTime now = LocalDateTime.now();
-
-    confMap.put("주간 인기매장 업데이트", new JobParameter<>(now, LocalDateTime.class));
-    JobParameters jobParameters = new JobParameters(confMap);
-
-    try {
-      jobLauncher.run(popularShopBatch.popularShopJob(jobRepository,transactionManager, dataSource), jobParameters);
-    } catch (JobExecutionAlreadyRunningException | JobInstanceAlreadyCompleteException
-             | JobParametersInvalidException | org.springframework.batch.core.repository.JobRestartException e) {
-
-      log.error(e.getMessage());
-    }
   }
 }
