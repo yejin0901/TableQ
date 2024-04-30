7a15734a564e3e332c745792450169faadb92766
Hako99 <154326391+Hako99@users.noreply.github.com>
2024-04-30 16:14:07 +0900

    Update Scheduler.java

--- src/main/java/com/project/team11_tabling/global/scheduler/Scheduler.java
+++ src/main/java/com/project/team11_tabling/global/scheduler/Scheduler.java
@@ -19,7 +19,7 @@ public class Scheduler {
   private final ApplicationEventPublisher eventPublisher;
 
 
-  @Scheduled(fixedDelay = 100000)
+  @Scheduled(fixedDelay = 50000)
   public void addAvailableSeat() {
     log.info("addAvailableSeat");
 
