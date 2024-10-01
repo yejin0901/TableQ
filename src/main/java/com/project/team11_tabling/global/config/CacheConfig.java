fb5d9db6dae831c69835aeed6aad898579bebe83
Yejin <ggg06142@gmail.com>
2024-10-02 00:04:07 +0900

    CacheConfig TTL 수정

--- src/main/java/com/project/team11_tabling/global/config/CacheConfig.java
+++ src/main/java/com/project/team11_tabling/global/config/CacheConfig.java
@@ -8,4 +8,16 @@ import org.springframework.context.annotation.Configuration;
 @EnableCaching
 public class CacheConfig {
 
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
+
 }
